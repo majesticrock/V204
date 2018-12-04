a29324b70fef619cb3c7ee15d8de3bd050a040f1
rope1505 <rpelkner@googlemail.com>
2018-12-04 16:05:50 +0100

    Korrektur1

---
+++ abweichung.py
@@ -0,0 +1,9 @@
+import numpy as np 
+import math
+import pandas as pd
+from uncertainties import ufloat
+from uncertainties import unumpy as unp
+
+print(1- ufloat(113,0)/ufloat(121,9))
+print(1- ufloat(235,0)/ufloat(187,17))
+print(1- ufloat(20,0)/ufloat(17.7,1.2))
